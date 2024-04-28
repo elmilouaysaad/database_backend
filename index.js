206811a516f96346cc1dc3ec1734e58b99219f33
saad <proesaad@gmail.com>
2024-04-28 17:26:51 +0100

    hello

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3001
 require('dotenv').config();
+const port = process.env.PORT
 const client_model = require('./clientModel')
 const order_model = require('./orderModel')
 app.use(express.json())
