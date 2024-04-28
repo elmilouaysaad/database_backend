f35db7d7353d9cdbeabcb23c9252884d35193026
saad <proesaad@gmail.com>
2024-04-28 16:15:21 +0100

    hello

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client_model = require('./clientModel')
 const order_model = require('./orderModel')
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://database-frontend.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
